3b211027a28c489fbd5ee39de81454027b1b48aa
Alexis Vespa <alexis_1722@hotmail.es>
2026-06-03 17:30:39 -0300

    Fix: Configurar Vite para múltiples entry points

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   server: {
@@ -6,5 +7,11 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+      },
+    },
   },
 })
